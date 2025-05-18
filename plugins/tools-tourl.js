f5fd0c12e96f3de86c0f29a6162681649aadfb9b
Ado <adonaylester85@gmail.com>
2025-05-18 11:58:40 -0600

    Actualizar tools-tourl.js

--- plugins/tools-tourl.js
+++ plugins/tools-tourl.js
@@ -1,44 +1,63 @@
-import uploadFile from '../lib/uploadFile.js'
-import uploadImage from '../lib/uploadImage.js'
-import fetch from 'node-fetch'
+import fetch from "node-fetch";
+import crypto from "crypto";
+import { FormData, Blob } from "formdata-node";
+import { fileTypeFromBuffer } from "file-type";
 
-let handler = async (m) => {
-  let q = m.quoted ? m.quoted : m
-  let mime = (q.msg || q).mimetype || ''
-  if (!mime) return conn.reply(m.chat, '🚩 Responde a una *Imagen* o *Vídeo.*', m, rcanal)
-  await m.react('🕓')
+let handler = async (m, { conn }) => {
+  let q = m.quoted ? m.quoted : m;
+  let mime = (q.msg || q).mimetype || '';
+  if (!mime) return conn.reply(m.chat, `${emoji} Por favor, responde a un archivo válido (imagen, video, etc.).`, m);
+
+  await m.react(rwait);
+
   try {
-  let media = await q.download()
-  let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
-  let link = await (isTele ? uploadImage : uploadFile)(media)
-  let img = await (await fetch(`${link}`)).buffer()
-  let txt = `*乂  T E L E G R A P H  -  U P L O A D E R*\n\n`
-      txt += `  *» Enlace* : ${link}\n`
-      txt += `  *» Acortado* : ${await shortUrl(link)}\n`
-      txt += `  *» Tamaño* : ${formatBytes(media.length)}\n`
-      txt += `  *» Expiración* : ${isTele ? 'No expira' : 'Desconocido'}\n\n`
-      txt += `🚩 *${textbot}*`
+    let media = await q.download();
+    let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
+    let link = await catbox(media);
 
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
-await m.react('✅')
-} catch {
-await m.react('✖️')
-}}
-handler.help = ['tourl']
-handler.tags = ['tools']
-handler.command = /^(tourl|upload)$/i
-export default handler
+    let txt = `*乂 C A T B O X - U P L O A D E R 乂*\n\n`;
+    txt += `*» Enlace* : ${link}\n`;
+    txt += `*» Tamaño* : ${formatBytes(media.length)}\n`;
+    txt += `*» Expiración* : ${isTele ? 'No expira' : 'Desconocido'}\n\n`;
+    txt += `> *${dev}*`;
+
+    await conn.sendFile(m.chat, media, 'thumbnail.jpg', txt, m, fkontak);
+
+    await m.react(done);
+  } catch {
+    await m.react(error);
+  }
+};
+
+handler.help = ['tourl2'];
+handler.tags = ['transformador'];
+handler.command = ['catbox', 'tourl2'];
+export default handler;
 
 function formatBytes(bytes) {
   if (bytes === 0) {
     return '0 B';
   }
-  const sizes = ['B', 'KB', 'MB', 'GB', 'TB']
-  const i = Math.floor(Math.log(bytes) / Math.log(1024))
+  const sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
+  const i = Math.floor(Math.log(bytes) / Math.log(1024));
   return `${(bytes / 1024 ** i).toFixed(2)} ${sizes[i]}`;
 }
 
-async function shortUrl(url) {
-	let res = await fetch(`https://tinyurl.com/api-create.php?url=${url}`)
-	return await res.text()
-}
+async function catbox(content) {
+  const { ext, mime } = (await fileTypeFromBuffer(content)) || {};
+  const blob = new Blob([content.toArrayBuffer()], { type: mime });
+  const formData = new FormData();
+  const randomBytes = crypto.randomBytes(5).toString("hex");
+  formData.append("reqtype", "fileupload");
+  formData.append("fileToUpload", blob, randomBytes + "." + ext);
+
+  const response = await fetch("https://catbox.moe/user/api.php", {
+    method: "POST",
+    body: formData,
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36",
+    },
+  });
+
+  return await response.text();
