996b6fad1e0d6a3a5c222934efa949204df64083
Ado <adonaylester85@gmail.com>
2025-05-18 12:26:26 -0600

    Actualizar main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,9 +1,22 @@
-let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Wirkダーク;;\nFN:Irokz Dal ダーク\nORG:Wirk ダーク\nTITLE:\nitem1.TEL;waid=50493732693:50493732693\nitem1.X-ABLabel:Wirk ダーク⁩\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Irokz Dal ダーク\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'Dev wirk ダーク⁩', contacts: [{ vcard }] }}, {quoted: m})
-}
-handler.help = ['owner']
-handler.tags = ['main']
-handler.command = ['owner', 'creator', 'creador', 'dueño'] 
+let handler = async (m, { conn }) => {
+  let vcard = `BEGIN:VCARD
+VERSION:3.0
+N:;Wirk;;;
+FN:Wirk
+ORG:
+TEL;type=CELL;waid=50493732693:50493732693
+END:VCARD`;
+  
+  await conn.sendMessage(m.chat, {
+    contacts: {
+      displayName: 'Wirk',
+      contacts: [{ vcard }]
+    }
+  }, { quoted: m });
+};
 
-export default handler
+handler.help = ['owner'];
+handler.tags = ['main'];
+handler.command = ['owner', 'creator', 'creador', 'dueño'];
+
+export default handler;
