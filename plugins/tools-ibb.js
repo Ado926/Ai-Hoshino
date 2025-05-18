dde07852874efc1f3f0f8e40fad2e22da36aefd4
Ado <adonaylester85@gmail.com>
2025-05-18 16:01:58 -0600

    Update tools-ibb.js

--- plugins/tools-ibb.js
+++ plugins/tools-ibb.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn }) => {
   let mime = (q.msg || q).mimetype || ''
   
   if (!mime.startsWith('image/')) {
-    return m.reply('🚩 Responde a una *Imagen.*')
+    return m.reply(`${emoji} rᥱs⍴᥆ᥒძᥲ ᥲ ᥙᥒᥲ *іmᥲgᥱᥒ.*`)
   }
   await m.react('🕓')
 
@@ -33,15 +33,15 @@ let handler = async (m, { conn }) => {
         txt += `  *» File* : ${q.filename || 'x.jpg'}\n`
         txt += `  *» Extension* : ${api.data.data.image.extension}\n`
         txt += `  *» Delete* : ${api.data.data.delete_url}\n\n`
-        txt += `🚩 *${textbot}*`
-    await conn.sendFile(m.chat, api.data.data.url, 'ibb.jpg', txt, m, null, rcanal)
+        txt += `> *${dev}*`
+    await conn.sendFile(m.chat, api.data.data.url, 'ibb.jpg', txt, m, fkontak)
     await m.react('✅')
   } else {
     await m.react('✖️')
   }
 }
-handler.tags = ['tools']
+handler.tags = ['transformador']
 handler.help = ['ibb']
-handler.command = /^(ibb)$/i
+handler.command = ['ibb', 'tourl3']
 handler.register = true 
 export default handler
