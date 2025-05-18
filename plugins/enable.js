e175121a57bf3b4770b8719212eff6cc145aca0f
Ado <adonaylester85@gmail.com>
2025-05-18 11:27:48 -0600

    Update enable.js

--- plugins/enable.js
+++ plugins/enable.js
@@ -1,4 +1,3 @@
-
 let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isROwner }) => {
   let isEnable = /true|enable|(turn)?on|1/i.test(command)
   let chat = global.db.data.chats[m.chat]
@@ -6,23 +5,22 @@ let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isR
   let bot = global.db.data.settings[conn.user.jid] || {}
   let type = (args[0] || '').toLowerCase()
   let isAll = false, isUser = false
+
   switch (type) {
-  case 'welcome':
+    case 'welcome':
     case 'bv':
     case 'bienvenida':
-      if (!m.isGroup) {
-        if (!isOwner) {
-          global.dfail('group', m, conn)
-          throw false
-        }
-      } else if (!isAdmin) {
+      if (!m.isGroup && !isOwner) {
+        global.dfail('group', m, conn)
+        throw false
+      } else if (m.isGroup && !isAdmin) {
         global.dfail('admin', m, conn)
         throw false
       }
       chat.bienvenida = isEnable
       break
-     
-     case 'autoread':
+
+    case 'autoread':
     case 'autoleer':
       isAll = true
       if (!isROwner) {
@@ -34,68 +32,65 @@ let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isR
 
     case 'document':
     case 'documento':
-    isUser = true
-    user.useDocument = isEnable
-    break
- 
+      isUser = true
+      user.useDocument = isEnable
+      break
+
     case 'antilink':
-      if (m.isGroup) {
-        if (!(isAdmin || isOwner)) {
-          global.dfail('admin', m, conn)
-          throw false
-        }
+      if (m.isGroup && !(isAdmin || isOwner)) {
+        global.dfail('admin', m, conn)
+        throw false
       }
       chat.antiLink = isEnable
       break
-      
-      case 'nsfw':
-      case 'modohorny':
-       if (m.isGroup) {
-         if (!(isAdmin || isOwner)) {
-           global.dfail('admin', m, conn)
-            throw false
-           }}
-    chat.nsfw = isEnable          
-    break
 
-     case 'antiarabes':
-     case 'antinegros':
-       if (m.isGroup) {
-         if (!(isAdmin || isOwner)) {
-           global.dfail('admin', m, conn)
-           throw false
-         }}
-       chat.onlyLatinos = isEnable  
-       break
+    case 'nsfw':
+    case 'modohorny':
+      if (m.isGroup && !(isAdmin || isOwner)) {
+        global.dfail('admin', m, conn)
+        throw false
+      }
+      chat.nsfw = isEnable
+      break
+
+    case 'antiarabes':
+    case 'antinegros':
+      if (m.isGroup && !(isAdmin || isOwner)) {
+        global.dfail('admin', m, conn)
+        throw false
+      }
+      chat.onlyLatinos = isEnable
+      break
+
+    case 'antiprivado':
+    case 'antiprivate':
+      if (!isROwner) {
+        global.dfail('rowner', m, conn)
+        throw false
+      }
+      bot.antiPrivate = isEnable
+      break
+
     default:
       if (!/[01]/.test(command)) return m.reply(`
 *🚩 Ingresa una opción para habilitar o deshabilitar*
 
-*≡ Lista de opciones*
-*Tipo :* welcome
-*Descripción :* Des/Activa la *Bienvenida* y *Despedida* para Grupos
+*≡ Lista de opciones disponibles:*
+— *welcome* ➤ Activa bienvenida/grupo
+— *nsfw* ➤ Activa comandos NSFW
+— *antilink* ➤ Activa anti-links en grupos
+— *antiarabes* ➤ Activa anti-no latinos
+— *autoread* ➤ Auto leer mensajes
+— *document* ➤ Descargar en documentos
+— *antiprivado* ➤ Bloquea mensajes privados al bot
 
-*Tipo :* nsfw 
-*Descripción :* Des/Activa los comandos *NSFW* para Grupos
-
-*Tipo :* antiarabes 
-*Descripción :* Des/Activa el *AntiArabes* para Grupos
-
-*Tipo :* antilink 
-*Descripción :* Des/Activa el *AntiLink* para Grupos
-
-*Tipo :* autoread 
-*Descripción :* Des/Activa el *AutoRead* para el Bot
-
-*Tipo :* document 
-*Descripción :* Des/Activa la *Descarga En Documentos* para el Usuario
-
-*• Ejemplo:*
-*- ${usedPrefix + command}* welcome
+*• Ejemplo:* 
+> ${usedPrefix + command} antiprivado
 `.trim())
       throw false
   }
-  m.reply(`La función *${type}* se *${isEnable ? 'activó' : 'desactivó'}* ${isAll ? 'para este bot' : isUser ? '' : 'para este chat'}`)
+
+  m.reply(`La función *${type}* fue *${isEnable ? 'activada' : 'desactivada'}* ${isAll ? 'globalmente' : isUser ? 'para ti' : 'en este chat'}`)
 }
 
 handler.help = ['enable', 'disable']
